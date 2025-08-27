2953701454b0abc8b84b737276587b51f7663659
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-08-27 10:00:02 +0300

    init express and handlebars

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import handlebars from "express-handlebars";
+
+const app = express();
+
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+
+app.set("view engine", hbs);
+
+app.set("views", "./src/views");
+
+app.listen(5000, () =>
+    console.log("The server is listening on http://localhost:5000...")
+);
