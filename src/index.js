9a9fd46bdc7e8150ca993a086446fcf5942e1e49
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-08-27 10:13:40 +0300

    add home page, and change the static folder's path

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import handlebars from "express-handlebars";
 
 const app = express();
 
+app.use(express.static("./src/static"));
+
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 
 app.set("view engine", "hbs");
 
 app.set("views", "./src/views");
+
+app.get("/", (req, res) => {
+    res.render("home", { layout: false });
+});
 
 app.listen(5000, () =>
     console.log("The server is listening on http://localhost:5000...")
