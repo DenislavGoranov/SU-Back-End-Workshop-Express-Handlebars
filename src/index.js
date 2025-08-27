ba8e9ffe24427fe5a20aff2244c04031dee19d5c
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-08-27 10:03:43 +0300

    update index.js path
    update index.js path because of an error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 
-app.set("view engine", hbs);
+app.set("view engine", "hbs");
 
 app.set("views", "./src/views");
 
