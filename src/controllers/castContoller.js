59e276d29ce40083d0624f3ab808a7b4774b277b
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-08-29 15:06:34 +0300

    add page for casts creating

--- src/controllers/castContoller.js
+++ src/controllers/castContoller.js
@@ -2,4 +2,8 @@ import express from "express";
 
 const castController = express.Router();
 
+castController.get("/cast/create", (req, res) => {
+    res.render("cast-create");
+});
+
 export default castController;
