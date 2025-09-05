54dee4aa1191d158dd44b2e3c2c9f57f59ea90e1
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-05 11:55:06 +0300

    getToken util

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,23 +2,17 @@ import User from "../Models/User.js";
 import jsonwebtoken from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { JWTsecret } from "../config/constants.js";
+import { getToken } from "../utils/userUtils.js";
 
 export default {
     async register(userData) {
-        const user = await User.create(userData);
-
         if (userData.password !== userData.rePassword) {
             throw new Error("Passwords Missmatch!");
         }
 
-        const payload = {
-            id: user.id,
-            email: user.email,
-        };
+        const user = await User.create(userData);
 
-        const token = jsonwebtoken.sign(payload, JWTsecret, {
-            expiresIn: "1h",
-        });
+        const token = getToken(user);
 
         return token;
     },
@@ -35,14 +29,7 @@ export default {
             throw new Error("Invalid Email or Password");
         }
 
-        const payload = {
-            id: user.id,
-            email: user.email,
-        };
-
-        const token = jsonwebtoken.sign(payload, JWTsecret, {
-            expiresIn: "1h",
-        });
+        const token = getToken(user);
 
         return token;
     },
---
+++ src/utils/userUtils.js
@@ -0,0 +1,12 @@
+export const getToken = (user) => {
+    const payload = {
+        id: user.id,
+        email: user.email,
+    };
+
+    const token = jsonwebtoken.sign(payload, JWTsecret, {
+        expiresIn: "1h",
+    });
+
+    return token;
+};
