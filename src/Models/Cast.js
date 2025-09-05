786e3cb98885b5ba3925f24931a16c2c7febf4e3
DenislavGoranov <denislavgoranov2005@abv.bg>
2025-09-05 12:12:07 +0300

    creating cast regex

--- src/Models/Cast.js
+++ src/Models/Cast.js
@@ -1,10 +1,38 @@
 import { Schema, model } from "mongoose";
 
 const castSchema = new Schema({
-    title: String,
-    age: Number,
-    born: String,
-    imageUrl: String,
+    title: {
+        type: String,
+        minLength: [5, "Name could be at least 5 characters long!"],
+        validate: [
+            /^[a-zA-Z ]+$/,
+            "Name can only contain English letters and spaces",
+        ],
+    },
+    age: {
+        type: Number,
+        minLength: [1, "Actor have to be 1 year old at least!"],
+        maxLength: [120, "Actor can/'t be older that 120 years!"],
+    },
+    born: {
+        type: String,
+        minLength: [
+            10,
+            "The actor's birthplace must be at least 3 characters long!",
+        ],
+        validate: [
+            /^[a-zA-Z ]+$/,
+            "Actor's born place can only contain English letters and spaces",
+        ],
+    },
+    imageUrl: {
+        type: String,
+        validate: [
+            /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|bmp|webp|svg))$/i,
+            "Invalid image URL",
+        ],
+        required: true,
+    },
 });
 
 const Cast = model("Cast", castSchema);
--- src/Models/User.js
+++ src/Models/User.js
@@ -4,14 +4,12 @@ import bcrypt from "bcrypt";
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
         unique: true,
         validate: [/@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+$/, "Invalid email!"],
         minLength: [10, "Email should be at least 10 characters long!"],
     },
     password: {
         type: String,
-        required: true,
         validate: [/^[a-zA-Z0-9]+$/, "Password should be alphanumeric!"],
         minLength: [6, "Password should be at least 6 characters long!"],
     },
